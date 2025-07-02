24dd3edc591586c07271407a388870ec30c35f5b
unmatched78 <marvinavi24@gmail.com>
2025-07-02 20:18:24 +0200

    rest+auth

--- users/views.py
+++ users/views.py
@@ -1,8 +1,49 @@
-# users/views.py
-from rest_framework import viewsets, permissions
+# users/core/views.py
+import logging
+from rest_framework import viewsets, status, generics, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, BasePermission, AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import action, api_view, permission_classes, authentication_classes, api_view, permission_classes
 from django.contrib.auth import get_user_model
-from rest_framework import generics, permissions
+from .models import *
+from rest_framework import filters
 from .serializers import *
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from decimal import Decimal
+from django.db import transaction 
+User = get_user_model()
+# Module‐level logger
+logger = logging.getLogger(__name__)  # best practice for namespaced logging
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        # Call the base class to get the original tokens
+        data = super().validate(attrs)
+        # Attach your user info
+        data['user'] = {
+            'id':       self.user.id,
+            'username': self.user.username,
+            'email':    self.user.email,
+            # add more fields as needed
+        }
+        return data
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # --- Inject these claims into BOTH access & refresh tokens ---
+        token['user_id']     = user.id
+        return token
+    
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegistrationSerializer
